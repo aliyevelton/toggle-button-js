7b6133e2cba16fe287da29e71a0b168dc533f25b
Elton <68150154+aliyevelton@users.noreply.github.com>
2024-01-17 20:07:22 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const button = document.querySelector(".toggle-button");
 const circle = document.querySelector(".circle");
 const container = document.querySelector(".container");
+const radio_container = document.querySelector(".radio_circle");
+const radio_label = document.querySelector(".radio_label");
 
-button.addEventListener("click", () => {
+radio_label.addEventListener("click", radioHandler);
+
+function radioHandler() {
+    radio_container.classList.toggle('active');
     button.classList.toggle('active');
     container.classList.toggle('active');
-});
+};
+
+
+button.addEventListener("click", buttonHandler);
+
+function buttonHandler() {
+    radio_container.classList.toggle('active');
+    button.classList.toggle('active');
+    container.classList.toggle('active');
+};
+
